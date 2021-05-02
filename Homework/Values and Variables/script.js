262ea0e46762ddcd3f7bf6871d024ab253f408a6
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-02 14:34:29 +0300

    data types

---
+++ Homework/Values and Variables/script.js
@@ -0,0 +1,15 @@
+let country = 'Moldova';
+let continent = 'Eurasia';
+let population = '26580000';
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
