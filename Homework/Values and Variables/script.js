5c7cf96006094fb9205d18046fcf3b4cf41caba9
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-03 16:25:31 +0300

    homework

--- Homework/Values and Variables/script.js
+++ Homework/Values and Variables/script.js
@@ -1,6 +1,6 @@
 const country = 'Moldova';
 const continent = 'Eurasia';
-let population = '26580000';
+let population = '2658000';
 
 // console.log(country);
 // console.log(continent);
@@ -9,8 +9,24 @@ let population = '26580000';
 const isIsland = false;
 const language = 'Romanian';
 
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+console.log(population / 2);
+population++;
+console.log(population);
+population--;
+
+const FinlandPopulation = 6.0e+6;
+console.log(FinlandPopulation > population);
+
+const avarageCountryPopulation = 33.0e+6;
+console.log(population < avarageCountryPopulation);
+
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
+console.log(country + ' is in ' + continent + ', and its ' + population +' million people speak ' + language);
+
 
