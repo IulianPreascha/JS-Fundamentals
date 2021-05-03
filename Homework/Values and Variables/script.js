41dec11e240cf8d27f5cea826fbbc78321b386e2
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-03 15:00:20 +0300

    homework

--- Homework/Values and Variables/script.js
+++ Homework/Values and Variables/script.js
@@ -1,15 +1,16 @@
-let country = 'Moldova';
-let continent = 'Eurasia';
+const country = 'Moldova';
+const continent = 'Eurasia';
 let population = '26580000';
 
 // console.log(country);
 // console.log(continent);
 // console.log(population);
 
-let isIsland = false;
-let language;
+const isIsland = false;
+const language = 'Romanian';
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
