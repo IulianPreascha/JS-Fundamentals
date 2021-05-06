06c115e92717d661df508500c0c8ac15406c12be
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-06 10:01:07 +0300

    logical operations

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -263,4 +263,22 @@ if(favourite !== 23) console.log(`Why not 23?`);
 const hasDriversLicense = true; // A
 const hasGoodVision = true // B
 
-console.log()
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+
+if(hasDriversLicense && hasGoodVision) {
+    console.log(`Sarah is able to drive!`)
+} else {
+    console.log(`Someone else should drive...`);
+}
+
+const isTired = true; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if(hasDriversLicense && hasGoodVision && !isTired) {
+    console.log(`Sarah is able to drive!`)
+} else {
+    console.log(`Someone else should drive...`);
+}
