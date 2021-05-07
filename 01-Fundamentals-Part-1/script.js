6922adceabf549bef38b625d9e3fddc8960b971d
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-07 12:33:33 +0300

    the conditional

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -321,7 +321,6 @@ if(day === `monday`) {
 } else {
     console.log(`Not a valid day`);
 }
-*/
 
 if(23 > 10) {
     const str = `23 is bigger`;
@@ -329,3 +328,21 @@ if(23 > 10) {
 
 const me = `Jonas`;
 console.log(`I'm ${2037 - 1991} years old ${me}`);
+*/
+
+const age = 23;
+// age >= 18 ? console.log(`I like to drink wine 🍷`) : 
+// console.log(`I like to drink water 💧`);
+
+const drink = age >= 18 ? `wine 🍷` : `water 💧`;
+console.log(drink);
+
+let drink2;
+if(age >= 18) {
+    drink2 = `wine 🍷`;
+} else {
+    drink2 = `water 💧`;
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? `wine 🍷` : `water 💧`}`);
