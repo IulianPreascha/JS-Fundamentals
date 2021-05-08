e68e5651c0ba1ef8efc944f0896c059ae381f021
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-08 13:15:21 +0300

    FINSIH - 01-FUNDAMENTALS

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -328,7 +328,6 @@ if(23 > 10) {
 
 const me = `Jonas`;
 console.log(`I'm ${2037 - 1991} years old ${me}`);
-*/
 
 const age = 23;
 // age >= 18 ? console.log(`I like to drink wine 🍷`) : 
@@ -346,3 +345,6 @@ if(age >= 18) {
 console.log(drink2);
 
 console.log(`I like to drink ${age >= 18 ? `wine 🍷` : `water 💧`}`);
+*/
+
+//! FINISH OF THE FIRST COURSE 
