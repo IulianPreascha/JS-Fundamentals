dc5948c02687d2cf5ac1eecbec8cbfda95aeacc8
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-13 18:48:12 +0300

    Object methods (Homework)

--- 01-Fundamentals-Part-2/HOMEWORK/script.js
+++ 01-Fundamentals-Part-2/HOMEWORK/script.js
@@ -150,13 +150,27 @@ const myCountry = {
     capital: 'Chisinau',
     language: 'Romanian',
     population: 2.3,
-    neighbours: ['Romania', 'Ucraine']
+    neighbours: ['Romania', 'Ucraine'],
+
+    describe: function() {
+        return `${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`;
+    },
+
+    checkIsland: function() {
+        return this.isIsland = this.neighbours.length === 0 ? true : false;
+    }
 };
 
-console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`);
+
+// console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`);
 
 myCountry.population += 2;
-console.log(myCountry.population);
+// console.log(myCountry.population);
 
 myCountry['population'] -= 2;
-console.log(myCountry.population);
+// console.log(myCountry.population);
+
+//! Object Methods
+
+console.log(myCountry.describe());
+console.log(myCountry.checkIsland());
