9cfef9a63d43eb802d00fe92ba485f8e064f4be3
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-13 18:36:40 +0300

    Object Methods

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -172,7 +172,7 @@ const jonasArray = [
     ['Michael', 'Peter', 'Steven']
 ];
 
-*/
+
 const jonas = {
     firstName: 'Jonas',
     lastName: 'Schmedtmann',
@@ -206,3 +206,45 @@ console.table(jonas);
 //? Challenge
 
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`);
+*/
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriveLicense: true,
+
+    // calcAge: function(birthYear) {
+    //     return 2037 - birthYear;
+    // }
+
+    // calcAge: function() {
+    //     // console.log(this);
+    //     return 2037 - this.birthYear;
+    // }
+
+    calcAge: function() {
+        this.age = 2037 - this.birthYear
+        return this.age;
+    },
+
+    getSummary: function() {
+        return `${this.firstName} is a ${this.calcAge()}-years old ${this.job}, and he has ${this.hasDriveLicense ? "a" : "no"} driver's license.`;
+    }
+};
+
+// console.log(jonas.calcAge());
+
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+
+//? Challenge
+
+console.log(jonas.getSummary());
+
+
+
+
