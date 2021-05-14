d246e6daf220884bbbc015f750e9dde9c04ca69a
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-15 00:23:11 +0300

    while loop

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -300,7 +300,7 @@ for(let i = 0; i < jonas.length; i++) {
     if(typeof jonas[i] === 'number') break;
     console.log(jonas[i], typeof jonas[i]);
 }
-*/
+
 
 const jonas = [
     'Jonas',
@@ -322,3 +322,26 @@ for (let exercise = 1; exercise <= 3; exercise++) {
         console.log(` Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️‍♀️`);
     }
 }
+*/
+
+//! WHILE LOOP
+
+// for (let rep = 1; rep <= 10; rep++) {
+//     console.log(`Lifting weight repetition ${rep} 🏋️‍♀️`);
+// }
+
+// let rep = 1;
+// while (rep <= 10) {
+//     console.log(`WHILE: Lifting weight repetition ${rep} 🏋️‍♀️`);
+//     rep++;
+// }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while(dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if(dice === 6) console.log(`Loop is about to end...`);
+}
+
