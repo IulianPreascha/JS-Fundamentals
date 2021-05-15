fd94ec4099f0b69b58b55ba3a31255cf44c74543
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-05-15 16:35:46 +0300

    coding challenge #4

--- 01-Fundamentals-Part-2/HOMEWORK/script.js
+++ 01-Fundamentals-Part-2/HOMEWORK/script.js
@@ -251,27 +251,25 @@ const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
 
-const calcTip = function(bill) {
+const calcTips = function(bill) {
     return bill >= 50 && bill <= 300 ? bill *= 0.15 : bill *= 0.2;
 }
 
-for(let i = 0; i < bills.length; i++) {
-    const tip = calcTip(bills[i]);
+for(i = 0; i < bills.length; i++) {
+    const tip = calcTips(bills[i]);
     tips.push(tip);
     totals.push(tip + bills[i]);
 }
-console.log(bills);
-console.log(tips);
-console.log(totals);
+
+console.log(bills, tips, totals);
 
 const calcAvarage = function(arr) {
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++) {
-        // sum = sum + arr[i];
+    sum = 0;
+    for(i = 0; i < arr.length; i++) {
         sum += arr[i];
     }
-    return sum / arr.length;
+    return sum = sum / arr.length;
 }
-console.log(calcAvarage([2,3,7]));
+
 console.log(calcAvarage(totals));
 console.log(calcAvarage(tips));
