0a3dd1919dd13ea93a43c03b1226576238bedb69
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-06-03 15:11:31 +0300

    handing click events

--- Dom/script.js
+++ Dom/script.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+// document.querySelector('.message').textContent = 'Correct Numbe! 🎉';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
