d09bb1f304933a635115e9c89a6a1b5a61a7abbd
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-06-09 18:31:10 +0300

    manipulate style

--- Dom/2/script.js
+++ Dom/2/script.js
@@ -4,7 +4,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log(`Button clicked ${[i + 1]}`);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
