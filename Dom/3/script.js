448c8659f06607a9e5dc315bc6fbaf46b06a25ab
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-06-15 22:11:55 +0300

    adaptation

--- Dom/3/script.js
+++ Dom/3/script.js
@@ -78,10 +78,10 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 10) {
       // Finish the game
       playing = false;
-      diceEl.classList.add('hidden');
+      // diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
