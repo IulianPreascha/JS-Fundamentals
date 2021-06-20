a85d75550061f70f8aaaf7a568e04644c3de7637
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-06-20 22:47:49 +0300

    how scoping works

--- BehindTheScene/1/script.js
+++ BehindTheScene/1/script.js
@@ -4,15 +4,25 @@ function calcAge(birthyear) {
   const age = 2037 - birthyear;
 
   function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthyear}`;
+    let output = `${firstName}, you are ${age}, born in ${birthyear}`;
     console.log(output);
 
     if (birthyear >= 1981 && birthyear <= 1996) {
       var millenial = true;
+      // Creating new variable with same name as outer scope's variable
+      const firstName = 'Steven';
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
     }
     console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
   }
   printAge();
 
