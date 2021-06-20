3091b1798389d50bf091526d51607609436d9cd6
IulianPreascha <79814115+IulianPreascha@users.noreply.github.com>
2021-06-20 22:39:25 +0300

    scoping in practice

--- BehindTheScene/1/script.js
+++ BehindTheScene/1/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function calcAge(birthyear) {
+  const age = 2037 - birthyear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthyear}`;
+    console.log(output);
+
+    if (birthyear >= 1981 && birthyear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    console.log(millenial);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
